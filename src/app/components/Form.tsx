56694d743b0d19e3906a1b3d85978ca550296423
Naif Otibi <nn.n@outlook.cl>
2024-10-11 19:39:50 +0300

    complete the progress logic (:

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { useState } from "react";
+import ProgressLimiter from "./ProgressLimiter";
 
 export default function Form() {
-  const [feedbackValue, setFeedbackValue] = useState("");
-  const [progressValue, setProgressValue] = useState();
-  const handleFeedbackChange = (e: React.FormEvent<HTMLTextAreaElement>) => {};
+  const [feedbackValue, setFeedbackValue] = useState<string>("");
+  const [progressValue, setProgressValue] = useState<number>(0);
+  const handleFeedbackChange = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    const feedbackValue = e.currentTarget.value;
+    setFeedbackValue(feedbackValue)
+    setProgressValue(feedbackValue.length);
+  };
   return (
     <div className="border p-4 rounded w-full">
       <div className="space-y-2">
@@ -23,8 +28,12 @@ export default function Form() {
             rows={4}
             cols={30}
             id="feedback"
-            className="w-full resize-none outline-none ring-black rounded-md border ring-0 focus-visible:ring-2 focus-visible:ring-ring p-3 text-sm"
+            disabled={Number(feedbackValue) > 100}
+            className="w-full disabled:cursor-not-allowed resize-none outline-none ring-black rounded-md border ring-0 focus-visible:ring-2 focus-visible:ring-ring p-3 text-sm"
           />
+        </div>
+        <div className="flex gap-2">
+            <ProgressLimiter value={progressValue} />
         </div>
       </form>
     </div>
